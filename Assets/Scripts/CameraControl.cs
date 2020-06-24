6e6101b2f2d8199c3fa0162879a395580c20c320
Ethan Roberts <theLameBrain@gmail.com>
2020-06-24 10:30:55 -0700

    Finished Camera

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -31,7 +31,7 @@ public class CameraControl : MonoBehaviour
             if (Input.GetAxisRaw("Mouse ScrollWheel") < 0f) { zoom = zoom + 0.5f; } //zoom out
         }
 
-        if (Input.GetMouseButton(2) || Input.GetMouseButton(1)) //Middle Mouse Button or Right Mouse Button
+        if (Input.GetMouseButton(1)) //Right Mouse Button Moves camera
         {
             if (Input.GetAxis("Mouse X") < 0) mx = Camera_Speed; //mouse moves left
             if (Input.GetAxis("Mouse X") > 0) mx = -Camera_Speed; //mouse moves right
@@ -39,24 +39,39 @@ public class CameraControl : MonoBehaviour
             if (Input.GetAxis("Mouse Y") > 0) my = -Camera_Speed; ; //mouse moves up
         }
 
+        if(Input.GetMouseButton(2)) //Middle mouse button rotates camera;
+        {
+            if (Input.GetAxis("Mouse X") < 0) Test_Target.transform.Rotate(Vector3.forward * 90 * Time.deltaTime); //rotate left
+            if (Input.GetAxis("Mouse X") > 0) Test_Target.transform.Rotate(-Vector3.forward * 90 * Time.deltaTime); //rotate right
+            if (Input.GetAxis("Mouse Y") < 0) zoom = zoom + 0.5f; //Zoom out
+            if (Input.GetAxis("Mouse Y") > 0) zoom = zoom - 0.5f; //zoom in
+    }
+
         //WASD or ArrowKeys
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) my = Camera_Speed;
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) mx = -Camera_Speed;
         if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) my = -Camera_Speed;
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) mx = Camera_Speed;
 
+        //Q & E or Delete & Insert keys to rotate camera
+        if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.Delete)) Test_Target.transform.Rotate(Vector3.forward * 90 * Time.deltaTime); //rotate left
+        if (Input.GetKey(KeyCode.E) || Input.GetKey(KeyCode.Insert)) Test_Target.transform.Rotate(-Vector3.forward * 90 * Time.deltaTime); //rotate right
+
         //camera bounding code. I place a test object... 
         Test_Target.transform.Translate(mx, my, 0, Space.Self);
         float tx = Test_Target.transform.position.x, ty = Test_Target.transform.position.y;
         float maxX = GameManager.GAME.room[GameManager.CURRENTROOM].GetComponent<Room>().xSize, maxY = GameManager.GAME.room[GameManager.CURRENTROOM].GetComponent<Room>().ySize;
-        if (tx > -2 && tx < maxX + 2 && ty > -2 && ty < maxY + 2) //... and if it is in bounds....
-        {
-            //..then I set the position of the real camera
-            Camera_Target.transform.Translate(mx, my, 0, Space.Self);
-            Camera.main.transform.LookAt(Camera_Target.transform.position, transform.up);
-            Camera.main.transform.localPosition = new Vector3(0, -zoom, -zoom);
-        }
-        //then I reset the positon of the test object
-        Test_Target.transform.position = Camera_Target.transform.position;
+        if (tx < -2) tx = -2;
+        if (tx > maxX) tx = maxX;
+        if (ty < -2) ty = -2;
+        if (ty > maxY) ty = maxY;
+        if (zoom < 2) zoom = 2;
+        if (zoom > 15) zoom = 15;
+        Test_Target.transform.position = new Vector3(tx, ty, Test_Target.transform.position.z);
+
+        //..then I set the position of the real camera                
+        Camera_Target.transform.position = Test_Target.transform.position; Camera_Target.transform.rotation = Test_Target.transform.rotation;
+        Camera.main.transform.LookAt(Camera_Target.transform.position, transform.up);
+        Camera.main.transform.localPosition = new Vector3(0, -zoom, -zoom);        
     }
 }
